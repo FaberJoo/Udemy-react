6b5202736dcff70594251fe97ff98bb002161db7
FaberJoo <79781818+FaberJoo@users.noreply.github.com>
2022-05-20 21:51:48 +0900

    form에도 css module 적용 및 버튼 미디어 쿼리 적용

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
 
-const FormInput = styled.div`
-    margin: 0.5rem 0;
+// const FormInput = styled.div`
+//   margin: 0.5rem 0;
 
-    & label {
-      font-weight: bold;
-      display: block;
-      margin-bottom: 0.5rem;
-      color: ${({invalid}) => invalid ? 'red' : 'black'}
-    }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${({ invalid }) => (invalid ? "red" : "black")};
+//   }
 
-    & input {
-      display: block;
-      width: 100%;
-      border: 1px solid ${({invalid}) => invalid ? 'red' : '#ccc'};
-      background: ${({invalid}) => invalid ? '#ffd7d7' : 'transparent'};
-      font: inherit;
-      line-height: 1.5rem;
-      padding: 0 0.25rem;
-    }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${({ invalid }) => (invalid ? "red" : "#ccc")};
+//     background: ${({ invalid }) => (invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-    & input:focus {
-      outline: none;
-      background: transparent;
-      border-color: #8b005d;
-    }
+//   & input:focus {
+//     outline: none;
+//     background: transparent;
+//     border-color: #8b005d;
+//   }
 
-    &.invalid input{
-      border-color: red;
-      background: #ffd7d7;
-    }
-`;
+//   &.invalid input {
+//     border-color: red;
+//     background: #ffd7d7;
+//   }
+// `;
 
 const CourseInput = ({ onAddGoal }) => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(false);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     } else {
@@ -49,22 +49,28 @@ const CourseInput = ({ onAddGoal }) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
     onAddGoal(enteredValue);
-    setEnteredValue('');
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormInput invalid={!isValid}>
-        <label >Course Goal</label>
-        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
-      </FormInput>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
+        <label>Course Goal</label>
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,5 @@
-import react from 'react';
-import styled from 'styled-components';
+import react from "react";
+import styled from "styled-components";
 
 // const Button = styled.button`
 //   width: 100%;
@@ -27,9 +27,9 @@ import styled from 'styled-components';
 //   }
 // `;
 
-import styles from  './Button.module.css';
+import styles from "./Button.module.css";
 
-const Button = props => {
+const Button = (props) => {
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
