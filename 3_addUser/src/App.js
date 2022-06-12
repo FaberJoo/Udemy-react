e9f8244f68f0d59e3fefb8e56b17bf2dcc34dd59
FaberJoo <79781818+FaberJoo@users.noreply.github.com>
2022-06-12 17:33:41 +0900

    addUser 이벤트  및 usersList 추가

--- 3_addUser/src/App.js
+++ 3_addUser/src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
-import AddUser from "./components/Users/AddUser";
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+const users = [
+	{
+		name: 'max',
+		age: '12',
+		id: Math.round(Math.random()).toString(),
+	},
+	{
+		name: 'dk',
+		age: '26',
+		id: Math.round(Math.random()).toString(),
+	},
+];
 
 function App() {
-  return (
-    <div>
-      <AddUser />
-    </div>
-  );
+	const [Users, setUsers] = useState(users);
+
+	const onAddUser = (userName, userAge) => {
+		setUsers((prevUsers) => {
+			return [
+				{
+					name: userName,
+					age: userAge,
+					id: Math.round(Math.random()).toString(),
+				},
+				...prevUsers,
+			];
+		});
+	};
+
+	return (
+		<div>
+			<AddUser onAddUser={onAddUser} />
+			<UsersList users={Users} />
+		</div>
+	);
 }
 
 export default App;
--- 3_addUser/src/components/Users/AddUser.js
+++ 3_addUser/src/components/Users/AddUser.js
@@ -1,24 +1,54 @@
-import React from "react";
-import Button from "../UI/Button";
-import Card from "../UI/Card";
-import styles from "./AddUser.module.css";
+import React, { useState } from 'react';
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import styles from './AddUser.module.css';
 
-const AddUser = (props) => {
-  const addUserHandler = (e) => {
-    e.preventDefault();
-  };
+const AddUser = ({ onAddUser }) => {
+	const [enteredUsername, setEnteredUsename] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
 
-  return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="username">Age(years)</label>
-        <input id="age" type="number" />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
-  );
+	const usenameChangeHandler = (e) => {
+		setEnteredUsename(e.target.value);
+	};
+
+	const ageChangeHandler = (e) => {
+		setEnteredAge(e.target.value);
+	};
+
+	const addUserHandler = (e) => {
+		e.preventDefault();
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			return;
+		}
+		if (+enteredAge < 1) {
+			return;
+		}
+		onAddUser(enteredUsername, enteredAge);
+		setEnteredAge('');
+		setEnteredUsename('');
+	};
+
+	return (
+		<Card className={styles.input}>
+			<form onSubmit={addUserHandler}>
+				<label htmlFor='username'>Username</label>
+				<input
+					id='username'
+					type='text'
+					value={enteredUsername}
+					onChange={usenameChangeHandler}
+				/>
+				<label htmlFor='username'>Age(years)</label>
+				<input
+					id='age'
+					type='number'
+					value={enteredAge}
+					onChange={ageChangeHandler}
+				/>
+				<Button type='submit'>Add User</Button>
+			</form>
+		</Card>
+	);
 };
 
 export default AddUser;
